afde807bee2b311bbd72b81a932d5906dedb232b
Stefan Guggisberg <sguggisb@adobe.com>
2019-03-25 15:23:59 +0100

    node 10 support; bump compiler version

--- node/deps/uv/src/unix/fs.c
+++ node/deps/uv/src/unix/fs.c
@@ -760,7 +760,8 @@ done:
 }
 
 static ssize_t uv__fs_copyfile(uv_fs_t* req) {
-#if defined(__APPLE__) && !TARGET_OS_IPHONE
+//#if defined(__APPLE__) && !TARGET_OS_IPHONE
+#if 0
   /* On macOS, use the native copyfile(3). */
   copyfile_flags_t flags;
 
